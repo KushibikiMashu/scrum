c5aae33c1b00f5b2a03c4ebea9dd32cf2f926677
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-03 00:14:36 +0900

    feat: コマンドのサンプルを追加

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,1 +1,10 @@
-export const add = (a: number, b: number) => a + b;
+import {input, select} from "@inquirer/prompts";
+
+(async() => {
+  const answers = {
+    inputted: await input({message: "What is your name?"}),
+    selected: await select({message: "What is your number?", choices: [{value: "1"}, {value: "2"}]}),
+  }
+
+  console.log(answers.inputted, answers.selected)
+})()
