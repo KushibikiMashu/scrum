9866c322bd9863238a75aa9377b327f3e5d2cb42
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-02 10:38:01 +0900

    feat: scrum team を追加

---
+++ packages/core/src/scrum/team/index.ts
@@ -0,0 +1,59 @@
+import {Member} from "@/company";
+
+export const ScrumMemberRole = {
+  ProductOwner: 'product_owner',
+  ScrumMaster: 'scrum_master',
+  Developer: 'developer',
+} as const;
+
+export type ScrumMemberRoleType = typeof ScrumMemberRole[keyof typeof ScrumMemberRole];
+
+export class ScrumTeam {
+  constructor(
+    public readonly productOwner: ProductOwner,
+    public readonly scrumMaster: ScrumMaster,
+    public readonly developers: Developer[],
+  ) {}
+}
+
+export class ProductOwner {
+  constructor(
+    public readonly roles: ScrumMemberRoleType[],
+    public readonly member: Member
+  ) {}
+
+  validate() {
+    if (this.roles.includes(ScrumMemberRole.ScrumMaster)) {
+      throw new Error('ProductOwner cannot be ScrumMaster');
+    }
+  }
+}
+
+export class ScrumMaster {
+  constructor(
+    public readonly roles: ScrumMemberRoleType[],
+    public readonly member: Member
+  ) {}
+
+  validate() {
+    if (this.roles.includes(ScrumMemberRole.ProductOwner)) {
+      throw new Error('ScrumMaster cannot be ProductOwner');
+    }
+  }
+}
+
+export class Developer {
+  constructor(
+    public readonly role: ScrumMemberRoleType = ScrumMemberRole.Developer,
+    public readonly member: Member
+  ) {}
+
+  validate() {
+    if (this.role === ScrumMemberRole.ProductOwner) {
+      throw new Error('Developer cannot be ProductOwner');
+    }
+    if (this.role === ScrumMemberRole.ScrumMaster) {
+      throw new Error('Developer cannot be ScrumMaster');
+    }
+  }
+}
