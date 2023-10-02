f0ec7ace79470245d1494ab029129cdeaca1967c
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-02 10:22:39 +0900

    feat: product をとりあえず追加

---
+++ packages/core/src/scrum/product/index.ts
@@ -0,0 +1,9 @@
+// export class Product {
+//   constructor(
+//     public readonly name: string,
+//     public readonly incrementSummary: IncrementSummary,
+//     public readonly productBacklog: ProductBacklog,
+//     public readonly goals: ProductGoal[],
+//   ) {
+//   }
+// }
