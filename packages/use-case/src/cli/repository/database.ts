2c6259482c6e3d3402360640b4c5a3f08f5d9be2
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-04 00:59:56 +0900

    feat: esbuild で use-case をビルドする

--- packages/use-case/src/cli/repository/database.ts
+++ packages/use-case/src/cli/repository/database.ts
@@ -1,11 +1,9 @@
-import { join, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
 
 import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
 import {DataBase} from "./schema";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
 const file = join(__dirname, 'db.json')
 
 const defaultData: DataBase = { posts: [] }
