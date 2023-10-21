f04a78a3bf7529b3d069742ab54df13681bdd614
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-22 03:05:42 +0900

    feat: error の redirect を切り出す

--- apps/web/src/app/[product]/[project]/page.tsx
+++ apps/web/src/app/[product]/[project]/page.tsx
@@ -1,10 +1,25 @@
 import {notFound, redirect} from "next/navigation";
 import {ProjectPageQueryService, ErrorReason} from "@panda-project/use-case";
 
-type Props =  {
+type Props = {
   params: {
     project: string
     product: string
+  }
+}
+
+const redirectOnError = ({data, error}) => {
+  const to404 = error?.reason === ErrorReason.InvalidProductName
+    || error?.reason === ErrorReason.InvalidProjectName
+    || error?.reason === ErrorReason.ProjectNotExists
+  const redirectToTop = data === null
+    || error?.reason === ErrorReason.ProductNotExists
+    || error?.reason === ErrorReason.UnknownError
+
+  if (to404) {
+    notFound()
+  } else if (redirectToTop) {
+    redirect('/')
   }
 }
 
@@ -12,22 +27,12 @@ export default async function ProductPage({params}: Props) {
   const {data, error} = await new ProjectPageQueryService().exec(
     {product: params.product, project: params.project}
   )
-
-  if (data === null || error?.reason === ErrorReason.ProductNotExists) {
-    redirect('/')
-  } else if (
-    error?.reason === ErrorReason.ProjectNotExists
-    // 存在しないプロダクト、プロジェクトの時
-    || data.product.name !== params.product
-    || data.project.name !== params.project
-  ) {
-    notFound()
-  }
+  redirectOnError({data, error})
 
   return (
     <div>
-    <p></p>
-    <p>a</p>
+      <p></p>
+      <p>a</p>
     </div>
   )
 }
