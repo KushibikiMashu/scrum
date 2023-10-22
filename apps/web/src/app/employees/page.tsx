e72a3ae515c8cb80f8aaee0e9d7ca93dadad3706
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-22 19:03:56 +0900

    feat: 社員一覧ページに別ページのリンクを追加

--- apps/web/src/app/employees/page.tsx
+++ apps/web/src/app/employees/page.tsx
@@ -1,9 +1,11 @@
 import { EmployeesPageQueryService} from "@panda-project/use-case";
 import {AddForm} from "./add-form";
 import {Employee} from "./employee";
+import Link from "next/link";
 
 export default async function EmployeesPage() {
   const {data} = await new EmployeesPageQueryService().exec()
+  const path = data.productName === null ? '/' : `/${data.productName}`
 
   return <div>
     <AddForm/>
@@ -12,5 +14,6 @@ export default async function EmployeesPage() {
         <Employee key={i} employeeId={employee.id!} employeeName={employee.name} />
       )}
     </ul>
+    <Link href={path}>top</Link>
   </div>
 }
--- packages/use-case/src/web/query-service/employees-page-query-service.ts
+++ packages/use-case/src/web/query-service/employees-page-query-service.ts
@@ -1,5 +1,5 @@
-import {Employee, EmployeeRepositoryInterface} from "@panda-project/core";
-import {EmployeeRepository} from "@/gateway";
+import {Employee, EmployeeRepositoryInterface, ProductName, ProductRepositoryInterface} from "@panda-project/core";
+import {EmployeeRepository, ProductRepository} from "@/gateway";
 import {Result} from "../types";
 
 type Dto = {
@@ -7,19 +7,28 @@ type Dto = {
     id: NonNullable<Employee['id']['value']>
     name: string
   }[]
+  productName: ProductName['value'] | null
 }
 
 export class EmployeesPageQueryService {
   constructor(
-    private readonly employeeRepository: EmployeeRepositoryInterface = new EmployeeRepository()
+    private readonly employeeRepository: EmployeeRepositoryInterface = new EmployeeRepository(),
+    private readonly productRepository: ProductRepositoryInterface = new ProductRepository(),
   ) {
   }
 
   async exec(): Promise<Result<Dto>> {
     const employees = await this.employeeRepository.findAll()
+    const product = await this.productRepository.fetch()
 
-    if (employees.length === 0) {
-      return {data: {employees: []}, error: null}
+    if (employees.length === 0 || product === null) {
+      return {
+        data: {
+          employees: [],
+          productName: null,
+        },
+        error: null
+      }
     }
 
     return {
@@ -27,9 +36,10 @@ export class EmployeesPageQueryService {
         employees: employees.map((employee) =>
           ({
             id: employee.id.value!,
-            name: employee.employeeName.getFullName()
+            name: employee.employeeName.getFullName(),
           })
-        )
+        ),
+        productName: product.name.value,
       },
       error: null
     }
