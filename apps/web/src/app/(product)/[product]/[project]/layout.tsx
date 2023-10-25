bae8001262043faf584b713cde5c222eff0b8f73
KushibikiMashu <kushibiki3413@gmail.com>
2023-10-26 01:48:17 +0900

    feat: サイドバーを追加

--- apps/web/src/app/(product)/[product]/[project]/layout.tsx
+++ apps/web/src/app/(product)/[product]/[project]/layout.tsx
@@ -1,27 +1,133 @@
+'use client'
+
+import { Disclosure } from '@headlessui/react'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import {
+  FolderIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline'
 import Link from "next/link";
 
-export default async function Layout({children}) {
+const navigation = [
+  {
+    name: 'Indie',
+    icon: FolderIcon,
+    current: false,
+    children: [
+      { name: 'スクラムチーム', href: '/scrum/indie/team' },
+      { name: 'プロダクトバックログ', href: '#' },
+      { name: 'スプリントバックログ', href: '#' },
+    ],
+  },
+  {
+    name: 'Sample Project',
+    icon: FolderIcon,
+    current: false,
+    children: [
+      { name: 'スクラムチーム', href: '#' },
+      { name: 'プロダクトバックログ', href: '#' },
+      { name: 'スプリントバックログ', href: '#' },
+    ],
+  },
+  { name: '社員一覧', href: '/employees', icon: UsersIcon, current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const MyLink = ({href, children}) => {
+  return <Link href={href}>{children}</Link>
+}
+
+export default function Example({children}) {
   return (
-    <div className="w-full">
-      <div className="flex h-full">
-        {/* menu */}
-        <div className="flex-shrink-0 px-4" style={{ width: 260 }}>
-          <ul>
-            <li>indie</li>
-            <ul className="ml-4">
-              <li>
-                <Link href="/scrum/indie/team">スクラムチーム</Link>
-              </li>
-              <li>プロダクトバックログ</li>
-              <li>スプリントバックログ</li>
-            </ul>
-          </ul>
-        </div>
-        {/* contents */}
-        <div className="flex-1 h-full max-w-full px-4">
-          {children}
-        </div>
+    <div className="h-screen flex">
+    <div className="flex w-[260px] flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+      <div className="flex h-16 shrink-0 items-center">
+        <Link href="/scrum">Scrum</Link>
       </div>
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="-mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  {!item.children ? (
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-700'
+                      )}
+                    >
+                      <item.icon className="h-6 w-6 shrink-0 text-gray-400" aria-hidden="true" />
+                      {item.name}
+                    </a>
+                  ) : (
+                    <Disclosure as="div" defaultOpen={item.name === 'Indie'}>
+                      {({ open }) => (
+                        <>
+                          <Disclosure.Button
+                            className={classNames(
+                              item.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                              'flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold text-gray-700'
+                            )}
+                          >
+                            <item.icon className="h-6 w-6 shrink-0 text-gray-400" aria-hidden="true" />
+                            {item.name}
+                            <ChevronRightIcon
+                              className={classNames(
+                                open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                                'ml-auto h-5 w-5 shrink-0'
+                              )}
+                              aria-hidden="true"
+                            />
+                          </Disclosure.Button>
+                          <Disclosure.Panel as="ul" className="mt-1 px-2">
+                            {item.children.map((subItem) => (
+                              <li key={subItem.name}>
+                                {/* 44px */}
+                                <Disclosure.Button
+                                  as="div"
+                                  className={classNames(
+                                    subItem.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                                    'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700'
+                                  )}
+                                >
+                                  <Link href={subItem.href}>
+                                  {subItem.name}
+                                  </Link>
+                                </Disclosure.Button>
+                              </li>
+                            ))}
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li className="-mx-6 mt-auto">
+            <a
+              href="#"
+              className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
+            >
+              <img
+                className="h-8 w-8 rounded-full bg-gray-50"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <span className="sr-only">Your profile</span>
+              <span aria-hidden="true">Tom Cook</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+      <div>{children}</div>
     </div>
   )
 }
