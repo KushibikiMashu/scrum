3822b2b6438861f67afd4023aa6b515393453808
KushibikiMashu <kushibiki3413@gmail.com>
2023-11-01 00:58:26 +0900

    feat: task list のデータをテストデータにする

--- apps/web/src/app/(product)/[product]/[project]/team/stats.tsx
+++ apps/web/src/app/(product)/[product]/[project]/team/stats.tsx
@@ -1,7 +1,7 @@
 const stats = [
   { name: '消化済みポイント', stat: '11（61.1%）' },
   { name: 'キャパシティ', stat: '18' },
-  { name: 'ベロシティ', stat: '17' },
+  { name: 'ベロシティ', stat: '17.8' },
 ]
 
 export default function Stats() {
@@ -13,13 +13,13 @@ export default function Stats() {
     <div>
       <div className="flex">
         <h3 className="text-base font-semibold leading-6 text-gray-600">スプリント8</h3>
-        <span className="text-sm leading-6 text-gray-600">（{toYearMonthDay(sprintStart)} ~ {toYearMonthDay(sprintEnd)}）</span>
+        <span className="text-xs leading-6 text-gray-600">（{toYearMonthDay(sprintStart)} ~ {toYearMonthDay(sprintEnd)}）</span>
       </div>
       <dl className="mt-4 grid grid-cols-1 gap-5 sm:grid-cols-3">
         {stats.map((item) => (
-          <div key={item.name} className="overflow-hidden rounded-lg bg-white px-4 py-5 border border-gray-300 sm:p-6">
-            <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-600">{item.stat}</dd>
+          <div key={item.name} className="overflow-hidden rounded-lg bg-white px-4 py-5 border border-gray-300 sm:p-4">
+            <dt className="truncate text-xs font-medium text-gray-500">{item.name}</dt>
+            <dd className="mt-1 text-2xl tracking-tight text-gray-600">{item.stat}</dd>
           </div>
         ))}
       </dl>
--- apps/web/src/app/(product)/[product]/[project]/team/task-list.tsx
+++ apps/web/src/app/(product)/[product]/[project]/team/task-list.tsx
@@ -1,25 +1,23 @@
 import { Fragment } from 'react'
 
-const locations = [
+const stories = [
   {
-    name: 'Edinburgh',
+    name: 'ブログを読む人がコメントできる',
+    point: 9,
     people: [
-      { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-      { name: 'Courtney Henry', title: 'Designer', email: 'courtney.henry@example.com', role: 'Admin' },
+      { title: 'コメントフォームを作成する', assignee: '竹内 太一',  status: '完了', point: '4' },
+      { title: 'APIを定義する', assignee: '佐々木 空',  status: 'Doing', point: '1' },
+      { title: 'バックエンドを実装する', assignee: '藤本 楓',  status: 'TODO', point: '4' },
     ],
   },
   {
-    name: 'Edinburgh',
+    name: 'ブログの著者が下書きを作成できる',
+    point: 9,
     people: [
-      { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-      { name: 'Courtney Henry', title: 'Designer', email: 'courtney.henry@example.com', role: 'Admin' },
-    ],
-  },
-  {
-    name: 'Edinburgh',
-    people: [
-      { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-      { name: 'Courtney Henry', title: 'Designer', email: 'courtney.henry@example.com', role: 'Admin' },
+      { title: '保存ボタンを作成する', assignee: '竹内 太一', status: 'TODO', point: '2' },
+      { title: 'APIを定義する', assignee: '佐々木 空', status: '完了', point: '1' },
+      { title: 'テーブル定義をする', assignee: '藤本 楓', status: '完了', point: '1' },
+      { title: 'ユースケースを実装する', assignee: '藤本 楓', status: '完了', point: '5' },
     ],
   },
 ]
@@ -44,56 +42,60 @@ export default function TaskList() {
           </button>
         </div>
       </div>
-      <div className="mt-8 flow-root">
+      <div className="mt-4 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-6">
             <table className="min-w-full">
-              <thead className="bg-white">
+              <thead className="bg-white sr-only">
               <tr>
-                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                  Name
+                <th>
+                  タイトル
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  Title
+                <th>
+                  担当者
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  Email
+                <th>
+                  ステータス
                 </th>
-                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  Role
+                <th>
+                  ポイント数
                 </th>
-                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                <th>
                   <span className="sr-only">Edit</span>
                 </th>
               </tr>
               </thead>
               <tbody className="bg-white">
-              {locations.map((location) => (
-                <Fragment key={location.name}>
+              {stories.map((story) => (
+                <Fragment key={story.name}>
                   <tr className="border-t border-gray-200">
                     <th
                       colSpan={5}
                       scope="colgroup"
                       className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
                     >
-                      {location.name}
+                      <div className="space-x-2">
+                        <span>{story.name}</span>
+                        <span className="text-xs font-medium">（{story.point}ポイント）</span>
+                      </div>
                     </th>
                   </tr>
-                  {location.people.map((person, personIdx) => (
+                  {story.people.map((person, personIdx) => (
                     <tr
-                      key={person.email}
+                      key={person.status}
                       className={classNames(personIdx === 0 ? 'border-gray-300' : 'border-gray-200', 'border-t')}
                     >
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                        {person.name}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.title}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.email}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{person.title}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.assignee}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.status}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.point}</td>
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                        <a href="#" className="text-gray-600 hover:text-gray-900">
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
+                        <button
+                          type="button"
+                          className="text-xs border border-gray-300 hover:bg-gray-50 rounded-md px-2.5 py-1.5"
+                        >
+                          編集する
+                        </button>
                       </td>
                     </tr>
                   ))}
