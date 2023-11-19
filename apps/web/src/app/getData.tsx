39944d7ac3f0f0e7eb52815b7c6d7a2bdf7e7646
KushibikiMashu <kushibiki3413@gmail.com>
2023-11-19 20:39:00 +0900

    feat: 不要なファイルを削除

--- apps/web/src/app/getData.tsx
+++
@@ -1,6 +0,0 @@
-import {ListScrumTeamScenario} from "@panda-project/use-case";
-
-export const getData = async () => {
-  const scenario = new ListScrumTeamScenario()
-  return await scenario.exec()
-}
