04374dc88240e909a22f5e06ea9e09f4f8c70774
KushibikiMashu <kushibiki3413@gmail.com>
2023-11-22 19:49:20 +0900

    docs: コメントを追加

--- packages/use-case/src/cli/scenario/index.ts
+++ packages/use-case/src/cli/scenario/index.ts
@@ -1,5 +1,10 @@
+export * from './create-team-scenario'
+
+// 入力がループなのでちょっと難しいかも
+// ループは cli で制御する。 use case では関与しない
 export * from './add-developer-scenario'
 export * from './remove-developer-scenario'
+
+// createOrUpdate を分けてから作る
 export * from './reselect-scrum-master-scenario'
-export * from './create-team-scenario'
 export * from './reselect-product-owner-scenario'
