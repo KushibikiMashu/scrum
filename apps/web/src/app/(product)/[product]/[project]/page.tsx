bf0f0b7116e6015131e4017050ff2b9b5a0fe800
KushibikiMashu <kushibiki3413@gmail.com>
2023-11-23 21:33:06 +0900

    feat: ProductPage をシンプルにする

--- apps/web/src/app/(product)/[product]/[project]/page.tsx
+++ apps/web/src/app/(product)/[product]/[project]/page.tsx
@@ -9,30 +9,6 @@ type Props = {
   }
 }
 
-const redirectOnError = ({data, error}: Awaited<ReturnType<ProjectPageQueryService['exec']>>) => {
-  const to404 = error?.reason === ErrorReason.InvalidProductName
-    || error?.reason === ErrorReason.InvalidProjectName
-    || error?.reason === ErrorReason.ProjectNotExists
-  const redirectToTop = data === null
-    || error?.reason === ErrorReason.ProductNotExists
-    || error?.reason === ErrorReason.UnknownError
-
-  if (to404) {
-    notFound()
-  } else if (redirectToTop) {
-    redirect('/')
-  }
-}
-
 export default async function ProductPage({params}: Props) {
-  const {data, error} = await new ProjectPageQueryService().exec(
-    {product: params.product, project: params.project}
-  )
-  redirectOnError({data, error})
-  assertDefined(data)
-  assertDefined(data?.product)
-
   redirect(`/${params.product}/${params.project}/team`)
-
-  return null
 }
