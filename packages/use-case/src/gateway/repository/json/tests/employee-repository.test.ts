c50f8dc39e4d70561fa5ddbe1f2469de43156251
KushibikiMashu <kushibiki3413@gmail.com>
2023-11-26 20:55:41 +0900

    feat: immemory の db の扱い方を正確にする

--- packages/use-case/src/gateway/repository/json/tests/employee-repository.test.ts
+++ packages/use-case/src/gateway/repository/json/tests/employee-repository.test.ts
@@ -1,13 +1,13 @@
 import { EmployeeRepository } from '../employee-repository';
 import {Employee, EmployeeId, EmployeeName} from "@panda-project/core";
 import {EmployeesSchema, Low} from "@/external/lowdb";
-import {getMockDb} from './helper/database';
-import {describe} from "node:test";
+import { setupDataBase} from './helper/database';
 
 let repository: EmployeeRepository;
 let mockDb: Low
-beforeEach(() => {
-  mockDb = getMockDb()
+beforeEach(async () => {
+  const {db} = await setupDataBase()
+  mockDb = db
   repository = new EmployeeRepository(mockDb);
 })
 
@@ -21,92 +21,95 @@ const fabricate = (data: Partial<EmployeesSchema> = null) => {
 }
 
 // テストデータをDBに保存する
-const fixture = (data: Partial<EmployeesSchema> = null) => {
+const fixture = async (data: Partial<EmployeesSchema> = null) => {
   const testData = fabricate(data)
-  mockDb.data.employees.push(testData)
+  await mockDb.read()
+  const {employees} = mockDb.data
+  employees.push(testData)
+  await mockDb.write()
   return testData
 }
 
-// describe('findByIdOrFail', () => {
-//   test('社員が存在する時は社員を返す', async () => {
-//     // arrange
-//     fixture()
-//     // act
-//     const result = await repository.findByIdOrFail(new EmployeeId(900));
-//     // assert
-//     expect(result.id.value).toBe(900);
-//     expect(result.employeeName.firstName).toBe('名前');
-//     expect(result.employeeName.familyName).toBe('苗字');
-//   })
-//
-//   test('社員が存在しない時はエラーを返す', async () => {
-//     const id = new EmployeeId(999)
-//     await expect(repository.findByIdOrFail(id)).rejects.toThrowError(`社員ID 999 は存在しません`);
-//   })
-// });
-//
-// describe('findAll', () => {
-//   test('社員が存在する時は社員を返す', async () => {
-//     mockDb.data.employees.push(fabricate({id: 900, first_name: '名前', family_name: '苗字'}))
-//     mockDb.data.employees.push(fabricate({id: 901, first_name: '名前2', family_name: '苗字2'}))
-//
-//     const actual = await repository.findAll();
-//     expect(actual.length).toBe(2);
-//     expect(actual[0].id.value).toBe(900);
-//     expect(actual[0].employeeName.firstName).toBe('名前');
-//     expect(actual[0].employeeName.familyName).toBe('苗字');
-//     expect(actual[1].id.value).toBe(901);
-//     expect(actual[1].employeeName.firstName).toBe('名前2');
-//     expect(actual[1].employeeName.familyName).toBe('苗字2');
-//   })
-//
-//   test('社員が存在しない時は空配列を返す', async () => {
-//     const actual = await repository.findAll();
-//     expect(actual.length).toBe(0);
-//   })
-// })
-//
-// describe('count', () => {
-//   test('社員が0人の時は0を返す', async () => {
-//     const result = await repository.count();
-//     expect(result).toBe(0);
-//   })
-//
-//   test('社員が2人の時は2を返す', async () => {
-//     mockDb.data.employees.push(fabricate({id: 900, first_name: '名前', family_name: '苗字'}))
-//     mockDb.data.employees.push(fabricate({id: 901, first_name: '名前2', family_name: '苗字2'}))
-//
-//     const result = await repository.count();
-//     expect(result).toBe(2);
-//   })
-// });
-//
-// describe('save', () => {
-//   test('新しい社員を保存したとき、新しい従業員を返す', async () => {
-//     const employee = new Employee(EmployeeId.createAsNull(), new EmployeeName('名前', '苗字'))
-//     const actual = await repository.save(employee);
-//
-//     expect(actual.id.value).toBe(1);
-//     expect(actual.employeeName.firstName).toBe('名前');
-//     expect(actual.employeeName.familyName).toBe('苗字');
-//     // db のチェック
-//     const record = mockDb.data.employees[0]
-//     expect(record['id']).toBe(1);
-//     expect(record['first_name']).toBe('名前');
-//     expect(record['family_name']).toBe('苗字');
-//   })
-//
-//   test('既存の社員を保存したとき、例外を投げる', async () => {
-//     const employee = new Employee(new EmployeeId(900), new EmployeeName('名前', '苗字'))
-//
-//     await expect(repository.save(employee))
-//       .rejects.toThrowError('社員IDはnullである必要があります。社員の更新は update メソッドを使ってください')
-//   })
-// })
+describe('findByIdOrFail', () => {
+  test('社員が存在する時は社員を返す', async () => {
+    // arrange
+    await fixture()
+    // act
+    const actual = await repository.findByIdOrFail(new EmployeeId(900));
+    // assert
+    expect(actual.id.value).toBe(900);
+    expect(actual.employeeName.firstName).toBe('名前');
+    expect(actual.employeeName.familyName).toBe('苗字');
+  })
+
+  test('社員が存在しない時はエラーを返す', async () => {
+    const id = new EmployeeId(999)
+    await expect(repository.findByIdOrFail(id)).rejects.toThrowError(`社員ID 999 は存在しません`);
+  })
+});
+
+describe('findAll', () => {
+  test('社員が存在する時は社員を返す', async () => {
+    await fixture({id: 900, first_name: '名前', family_name: '苗字'})
+    await fixture({id: 901, first_name: '名前2', family_name: '苗字2'})
+
+    const actual = await repository.findAll();
+    expect(actual.length).toBe(2);
+    expect(actual[0].id.value).toBe(900);
+    expect(actual[0].employeeName.firstName).toBe('名前');
+    expect(actual[0].employeeName.familyName).toBe('苗字');
+    expect(actual[1].id.value).toBe(901);
+    expect(actual[1].employeeName.firstName).toBe('名前2');
+    expect(actual[1].employeeName.familyName).toBe('苗字2');
+  })
+
+  test('社員が存在しない時は空配列を返す', async () => {
+    const actual = await repository.findAll();
+    expect(actual.length).toBe(0);
+  })
+})
+
+describe('count', () => {
+  test('社員が0人の時は0を返す', async () => {
+    const actual = await repository.count();
+    expect(actual).toBe(0);
+  })
+
+  test('社員が2人の時は2を返す', async () => {
+    await fixture({id: 900, first_name: '名前', family_name: '苗字'})
+    await fixture({id: 901, first_name: '名前2', family_name: '苗字2'})
+
+    const actual = await repository.count();
+    expect(actual).toBe(2);
+  })
+});
+
+describe('save', () => {
+  test('新しい社員を保存したとき、新しい従業員を返す', async () => {
+    const employee = new Employee(EmployeeId.createAsNull(), new EmployeeName('名前', '苗字'))
+    const actual = await repository.save(employee);
+
+    expect(actual.id.value).toBe(1);
+    expect(actual.employeeName.firstName).toBe('名前');
+    expect(actual.employeeName.familyName).toBe('苗字');
+    // db のチェック
+    const record = mockDb.data.employees[0]
+    expect(record['id']).toBe(1);
+    expect(record['first_name']).toBe('名前');
+    expect(record['family_name']).toBe('苗字');
+  })
+
+  test('既存の社員を保存したとき、例外を投げる', async () => {
+    const employee = new Employee(new EmployeeId(900), new EmployeeName('名前', '苗字'))
+
+    await expect(repository.save(employee))
+      .rejects.toThrowError('社員IDはnullである必要があります。社員の更新は update メソッドを使ってください')
+  })
+})
 
 describe('update', () => {
   test('既存の社員を更新したとき、更新した従業員を返す', async () => {
-    fixture()
+    await fixture()
     const employee = new Employee(new EmployeeId(900), new EmployeeName('新しい名前', '新しい苗字'))
     const actual = await repository.update(employee);
 
@@ -130,7 +133,7 @@ describe('update', () => {
 
 describe('delete', () => {
   test('既存の社員を削除できる', async () => {
-    fixture()
+    await fixture()
 
     const employee = new Employee(new EmployeeId(900), new EmployeeName('名前', '苗字'))
     await repository.delete(employee)
--- packages/use-case/src/gateway/repository/json/tests/helper/database.ts
+++ packages/use-case/src/gateway/repository/json/tests/helper/database.ts
@@ -1,4 +1,10 @@
 import {Low} from "lowdb";
 import {adapter, createDefaultData} from "@/external/lowdb";
 
-export const getMockDb = () => new Low(adapter, createDefaultData())
+export const setupDataBase =  async () => {
+  const db = new Low(adapter, createDefaultData())
+  await db.read();
+  db.data = createDefaultData()
+  await db.write();
+  return {db}
+}
